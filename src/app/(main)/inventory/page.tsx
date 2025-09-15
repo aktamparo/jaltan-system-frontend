9add6af23f5efcbc3746252db46f9ffbd8a43df6
aktamparo <aktamparo@addu.edu.ph>
2025-09-15 19:18:06 +0800

    comment out stock out component

--- src/app/(main)/inventory/page.tsx
+++ src/app/(main)/inventory/page.tsx
@@ -12,7 +12,7 @@ import {
 import Searchbar from "@/components/ui/searchbar";
 import StockInItem from "@/components/inventory/StockIn/StockInItem";
 import UpdateStockIn from "@/components/inventory/StockIn/UpdateStockIn";
-import StockOutItem from "@/components/inventory/StockOutItems/StockOutItem";
+//import StockOutItem from "@/components/inventory/StockOutItems/StockOutItem";
 import { DataTable as ViewTable } from "@/components/inventory/ViewBranchItems/user-view-table";
 import { InventoryItem, columns as ViewColumns } from "@/components/inventory/ViewBranchItems/columns";
 
@@ -47,7 +47,7 @@ export default function InventoryPage() {
         <div className="flex flex-row items-center gap-2 mt-2 ml-auto">
           <StockInItem />
           <UpdateStockIn/>
-          <StockOutItem />
+          {/* <StockOutItem /> */}
         </div>
       </div>
 
--- src/components/inventory/StockOutItems/EditStockOut.tsx
+++ src/components/inventory/StockOutItems/EditStockOut.tsx
@@ -1,57 +1,57 @@
-"use client";
+// "use client";
 
-import { useState } from "react";
-import type { StockOut } from "@/components/inventory/StockOutItems/CreateStockOut/columns";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+// import { useState } from "react";
+// import type { StockOut } from "@/components/inventory/StockOutItems/CreateStockOut/columns";
+// import { Button } from "@/components/ui/button";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
 
-interface EditStockOutProps {
-  item: StockOut;
-  onClose: () => void;
-  onSave: (stockDetails: any) => void;
-}
+// interface EditStockOutProps {
+//   item: StockOut;
+//   onClose: () => void;
+//   onSave: (stockDetails: any) => void;
+// }
 
-export default function StockOutDetailsModal({
-  onClose,
-  onSave,
-}: EditStockOutProps) {
-  const [quantity, setQuantity] = useState<number>(0);
-  const [uomName, setUomName] = useState<string>("kg");
+// export default function StockOutDetailsModal({
+//   onClose,
+//   onSave,
+// }: EditStockOutProps) {
+//   const [quantity, setQuantity] = useState<number>(0);
+//   const [uomName, setUomName] = useState<string>("kg");
 
-  return (
-    <div className="space-y-4">
+//   return (
+//     <div className="space-y-4">
 
-      <div className="space-y-2">
-        <Label htmlFor="quantity">Quantity</Label>
-        <Input
-          id="quantity"
-          type="number"
-          value={quantity}
-          onChange={(e) => setQuantity(Number(e.target.value))}
-        />
-      </div>
+//       <div className="space-y-2">
+//         <Label htmlFor="quantity">Quantity</Label>
+//         <Input
+//           id="quantity"
+//           type="number"
+//           value={quantity}
+//           onChange={(e) => setQuantity(Number(e.target.value))}
+//         />
+//       </div>
 
-      <div className="space-y-2"> 
-        <Label htmlFor="uom">Unit of Measurement</Label>
-        <Input
-          id="uom"
-          type="text"
-          value={uomName}
-          onChange={(e) => setUomName(e.target.value)}
-        />
-      </div>
+//       <div className="space-y-2"> 
+//         <Label htmlFor="uom">Unit of Measurement</Label>
+//         <Input
+//           id="uom"
+//           type="text"
+//           value={uomName}
+//           onChange={(e) => setUomName(e.target.value)}
+//         />
+//       </div>
 
-      <div className="flex justify-start gap-2">
-        <Button variant="outline" onClick={onClose}>
-          Cancel
-        </Button>
-        <Button
-          onClick={() => onSave({ quantity, uomName, uomId: "UOM1" })}
-        > 
-          Save
-        </Button>
-      </div>
-    </div>
-  );
-}
+//       <div className="flex justify-start gap-2">
+//         <Button variant="outline" onClick={onClose}>
+//           Cancel
+//         </Button>
+//         <Button
+//           onClick={() => onSave({ quantity, uomName, uomId: "UOM1" })}
+//         > 
+//           Save
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// }
