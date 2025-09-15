456705f4b1424e3a5d682db2b467b69bcbc24847
aktamparo <aktamparo@addu.edu.ph>
2025-09-15 22:00:15 +0800

    refactor: deleted stock out button component

--- src/app/(main)/inventory/page.tsx
+++ src/app/(main)/inventory/page.tsx
@@ -12,7 +12,6 @@ import {
 import Searchbar from "@/components/ui/searchbar";
 import StockInItem from "@/components/inventory/StockIn/StockInItem";
 import UpdateStockIn from "@/components/inventory/StockIn/UpdateStockIn";
-//import StockOutItem from "@/components/inventory/StockOutItems/StockOutItem";
 import { DataTable as ViewTable } from "@/components/inventory/ViewBranchItems/user-view-table";
 import { InventoryItem, columns as ViewColumns } from "@/components/inventory/ViewBranchItems/columns";
 
@@ -47,7 +46,6 @@ export default function InventoryPage() {
         <div className="flex flex-row items-center gap-2 mt-2 ml-auto">
           <StockInItem />
           <UpdateStockIn/>
-          {/* <StockOutItem /> */}
         </div>
       </div>
 
